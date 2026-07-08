f5d797558f24f42723628256fc1220b9cb977add
Pawandeep Singh <spawandeep2005@gmail.com>
2026-07-08 15:42:23 +0800

    Refactor main.py to use ChatOllama instead of ChatGroq, update dependencies in pyproject.toml for langchain and langchain-ollama, and enhance main function to initialize session and invoke agent with a sample query.

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import asyncio
 import os
 
 from dotenv import load_dotenv
-from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage
+from langchain_ollama import ChatOllama
 from langchain_mcp_adapters.tools import load_mcp_tools
-from langgraph.prebuilt import create_react_agent
+from langchain.agents import create_agent
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
 load_dotenv()
 
-llm = ChatGroq(model="llama-3.1-8b-instant")
+llm = ChatOllama(model="llama3.1:8b")
 
 stdio_server_params = StdioServerParameters(
     command= "python",
@@ -18,8 +19,17 @@ stdio_server_params = StdioServerParameters(
 )
 
 async def main():
-    print("Hello from mcp-crash-course!")
-
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Session initialized")
+            tools = await load_mcp_tools(session)
+        
+            agent = create_agent(llm, tools)
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
+      
+    
 
 if __name__ == "__main__":
     asyncio.run(main())
